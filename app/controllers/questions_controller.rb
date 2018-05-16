1ed09b72ab7692208f63438840e6e74b706e6652
spy <sp831031@gmail.com>
2018-05-16 15:19:39 +0800

    add questions new action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,8 @@ class QuestionsController < ApplicationController
   def index
     @questions = Questions.all.page(params[:page]).per(10)
   end
+
+  def new
+    @question = Question.new
+  end
 end
