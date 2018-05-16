96b1591207a922c0a8cc3c1348b04ae529e5038c
spy <sp831031@gmail.com>
2018-05-16 15:12:49 +0800

    setup expcept index action must login

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @questions = Questions.all.page(params[:page]).per(10)
   end
