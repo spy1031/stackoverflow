d309ba9c4cd06212aa0b6e88ad89b4f8877377c8
spy <sp831031@gmail.com>
2018-05-16 14:52:06 +0800

    rails generate question model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516055758) do
+ActiveRecord::Schema.define(version: 20180516064549) do
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "upvotes_count", default: 0
+    t.integer "favorites_count", default: 0
+    t.integer "solutions_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
