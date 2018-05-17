cbdfc7e1e1efb3ac3da4448674bac81be6da5e1f
Yipeechen <yipeechen0418@gmail.com>
2018-05-17 14:22:13 +0800

    fix question index action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @questions = Questions.all.page(params[:page]).per(10)
+    @questions = Question.all.page(params[:page]).per(10)
   end
 
   def new
