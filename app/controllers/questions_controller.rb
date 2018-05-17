90383c526b0b41aa002f19f697b9c9cc052e0450
Yipeechen <yipeechen0418@gmail.com>
2018-05-17 17:26:09 +0800

    add question create action and flash notice & alert

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,23 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
+  def create
+    @question = current_user.questions.new(question_params)
+    if @question.save
+      flash[:notice] = "問題發布成功"
+      redirect_to question_path(@question)
+    else
+      flash.now[:alert] = "問題發布失敗"
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(
+      :title,
+      :content)
+  end
 end
