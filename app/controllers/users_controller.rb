c9aa3d62a7d93d060c5ff96e09e41961ad361db0
beranemo <beranemo@gmail.com>
2018-05-19 11:32:47 +0800

    complete some data display in show page of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :show]
   
   def edit
   end
   
   def show
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
