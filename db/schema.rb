2504a1dcbc5f4a7f8bf2a36f5d8fd9084dc27920
beranemo <beranemo@gmail.com>
2018-05-19 12:37:01 +0800

    generate favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180519040748) do
+ActiveRecord::Schema.define(version: 20180519043538) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.integer "user_id", null: false
